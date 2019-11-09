8ebbe59c896f61fd3bccee9154726f7471e25b5e
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-11-08 22:31:38 -0400

    Add data to state

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,18 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      data: []
+    };
   }
 
   componentDidMount() {
     axios
       .get("http://localhost:5000/api/players")
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ data: res.data });
+      })
       .catch((err) => console.log(err));
   }
 
